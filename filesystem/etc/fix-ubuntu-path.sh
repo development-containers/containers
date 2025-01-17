854d7591d4c0b4eeee76c92100cb5585384655e8
double-daruma <96352666+double-daruma@users.noreply.github.com>
2025-01-17 20:22:42 +0000

    fix ubuntu

--- filesystem/etc/fix-ubuntu-path.sh
+++ filesystem/etc/fix-ubuntu-path.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
-set -euo pipefail
+set -xeuo pipefail
 
 # Devcontainer ubuntu bash overrides our path changes. Well. Then we just set them again
 
 
 declare -a paths=("/opt/nushell/" "/opt/typst" "/opt/zellij" "/opt/jujutsu" "/opt/starship" "/opt/deno" "/opt/intellij/bin" "/opt/cuelang" "/opt/gleam" "/opt/zola" "/opt/nickel" "/opt/taplo" "/opt/carapace")
 
+
+path_statement () {
+   echo -n 'if [[ "${PATH}" != *"' 
+    echo -n "$1" 
+    echo -n '"* ]]; then export PATH="${PATH}:' 
+    echo -n "$1"
+    echo '"; fi' 
+}
+
 for i in "${paths[@]}"
 do
-   echo "$i"
-    echo -n 'if [[ "${PATH}" != *"' >> /etc/bash.bashrc
-    echo -n "$i" >>  /etc/bash.bashrc
-    echo -n '"* ]]; then export PATH="${PATH}:' >> /etc/bash.bashrc
-    echo -n "$i" >>  /etc/bash.bashrc
-    echo '"; fi' >> /etc/bash.bashrc
+    echo "$i"
+    path_statement "$i" >> /etc/bash.bashrc
+    path_statement "$i" >> /home/codespace/.bashrc
+        path_statement "$i" >> /home/codespace/.profile
+
 done
+
+
+cat /etc/bash.bashrc
+cat /home/codespace/.bashrc
 
